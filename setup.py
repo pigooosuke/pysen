b0a5b2c1de79910cf15d0ecbcbf29a512647f449
pigooosuke <hifirole@gmail.com>
2023-04-26 18:19:21 +0900

    fix require mypy version < 1.0.0

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
             "flake8-bugbear",  # flake8 doesn't have a dependency for bugbear plugin
             "flake8>=3.7,<5",
             "isort>=4.3,<5.2.0",
-            "mypy>=0.770,<0.800",
+            "mypy>=0.770,<1.0.0",
         ],
     },
     package_data={"pysen": ["py.typed"]},
